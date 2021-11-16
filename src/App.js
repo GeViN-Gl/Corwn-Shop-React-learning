981f62935669f5e717165e7132a015c35b4bde36
Oleg Koval <gevin.gl@gmail.com>
2021-11-16 13:58:24 +0200

    Adding new sign-in user to Firebase DB checking if there is a user in our DB and reading users data from db

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header/header.component";
 import SignInAndSigUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 //import firebase auth to acees its methods
-import { auth } from "./firebase/firebase.util";
+import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
 import React, { Component } from "react";
 
@@ -22,11 +22,36 @@ class App extends Component {
 
   unsubscribeFromAuth = null;
 
+  // all fancy thigs we do as soon as we know that component exist on page
   componentDidMount() {
-    // onAuthStateChanged return firebase.unsubscribe, adn we can call it ti signout user
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(`user`, user);
+    // onAuthStateChanged return firebase.unsubscribe, adn we can call it to signout user
+    // also callback of auth must be async as createUserProfile is async fuction and will return promise
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        /* 
+        We can listen to a document with the onSnapshot() method.
+        An initial call using the callback provided creates a document snapshot immediately with the current contents of the single document.
+        Then, each time the contents change, another call updates the document snapshot.*/
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(), // .data() returns object with all data from db, so put all it in state
+              },
+            }
+            /* ,
+            () => {
+              //must leave here as notice, setState is async, to fire fn AFTER setstate complete must use callback as second arg
+              console.log(this.state);
+            } */
+          );
+        });
+      }
+      // 1st ELSE important, or null or exsis NOT both
+      // 2nd (just to remember) if else fired userAuth is null
+      else this.setState({ currentUser: userAuth });
     });
   }
 
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -23,6 +23,42 @@ const config = {
   measurementId: "G-VKEWDXCQ77",
 };
 
+/**
+ * Creates new User document in Firebase db if such user doesn't exist in our db
+ * @param {Object} userAuth Object created by firebase.auth() to identify user
+ * @param {Object | Object[]} additionalData Any additional data to pass in to base
+ * @returns {Object} userRef object from firestorm query
+ */
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; //if there is no Authed user, variable == null , do nothing
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // query to firestorm DB
+  // The queryReference object does not have the actual data of the collection or document.
+  // It instead has properties that tell us details about it, or the method to get the Snapshot object which gives us the data we are looking for.
+
+  const snapShot = await userRef.get();
+  // We get a documentSnapshot object from our documentReference object.
+  // The documentSnapshot object allows us to check if a document exists at this query using the .exists property which returns a boolean.
+  // So if such user dosn`t exist (no prev logins) we will create document(record) of this new authed user
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth; // take what we want to add to document from userAuth object
+    const createdAt = new Date(); //curr time
+
+    // then we try to store data and now we need to cath errors
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log(`😞 Error creatig user`, err.message);
+    }
+  }
+  return userRef; //to chain or mb for something else
+};
+
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
@@ -38,3 +74,53 @@ provider.setCustomParameters({ promt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+// QueryReference and QuerySnapshot
+
+// A query is a request we make to firestore to give us
+// something from the database.
+
+// Firestore returns us two types of objects: references and
+// snapshots. Of these objects, they can be either Document or
+// Collection versions.
+
+// Firestore will always return us these objects, even if nothing
+// exists at from that query.
+
+// QueryReference
+// A queryReference object is an object that represents the “current”
+// place in the database that we are querying.
+
+// We get them by calling either:
+// firestore.doc(‘/users/:userId’);
+// firestore.collections(‘/users’);
+
+// The queryReference object does not have the actual data of the
+// collection or document. It instead has properties that tell us details
+// about it, or the method to get the Snapshot object which gives us the
+// data we are looking for.
+
+// Document Reference vs CollectionReference
+// We use documentRef objects to perform our CRUD methods (create,
+// retrieve, update, delete). The documentRef methods are .set(), .get(),
+// .update() and .delete() respectively.
+// We can also add documents to collections using the collectionRef
+// object using the .add() method. // collectionRef.add({//value: prop})
+// We get the snapshotObject from the referenceObject using the .get()
+// method. ie. documentRef.get() or collectionRef.get()
+// documentRef returns a documentSnapshot object.
+// collectionRef returns a querySnapshot object.
+
+// DocumentSnapshot
+// We get a documentSnapshot object from our documentReference
+// object.
+// The documentSnapshot object allows us to check if a document exists
+// at this query using the .exists property which returns a boolean.
+// We can also get the actual properties on the object by calling
+// the .data() method, which returns us a JSON object of the document.QuerySnapshot
+// We get a querySnapshot object from our collectionReference object.
+// We can check if there are any documents in the collection by calling
+// the .empty property which returns a boolean.
+// We can get all the documents in the collection by calling the .docs
+// property. It returns an array of our documents as documentSnapshot
+// objects
